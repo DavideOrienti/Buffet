0577dd6dfb55abe0ba8814114e8802625161ec35
davide orienti <dav.orienti@stud.uniroma3.it>
2022-06-16 17:23:10 +0200

    creazione progetto base

---
+++ Buffet/src/test/java/it/uniroma3/siw/buffet/BuffetApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.buffet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuffetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
