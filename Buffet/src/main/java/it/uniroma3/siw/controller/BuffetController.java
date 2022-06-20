f88abdb0151c1516268b313981cba840e16816af
davide orienti <dav.orienti@stud.uniroma3.it>
2022-06-20 23:31:47 +0200

    aggiornamenti

--- Buffet/src/main/java/it/uniroma3/siw/controller/BuffetController.java
+++ Buffet/src/main/java/it/uniroma3/siw/controller/BuffetController.java
@@ -1,18 +1,23 @@
 package it.uniroma3.siw.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import it.uniroma3.siw.model.Buffet;
 import it.uniroma3.siw.service.BuffetService;
 import it.uniroma3.siw.validator.BuffetValidator;
 
+@Controller
 public class BuffetController {
 	
 	@Autowired
@@ -50,5 +55,41 @@ public class BuffetController {
 		else return "buffetForm.html";
 		
 	}
+	
+//	//@RequestMapping(value="/addBuffet", method = RequestMethod.GET)
+//	@GetMapping("/addBuffet")
+//	public String addFilm(Model model) {
+//		//logger.debug("addArtista");
+//		model.addAttribute("buffet", new Buffet());
+//		//
+//		//model.addAttribute("login",AuthenticationController.loggato);
+//		return "artistaForm.html";
+//	}
+//	
+	
+	//richiede tute le persone perche non specifico id
+	@GetMapping("/buffet")
+	public String getBuffet(Model model) {
+		List<Buffet> buffet = bs.FindAll();
+		model.addAttribute("buffet",buffet);
+		return "buffets.html";
+		
+		
+	}
+	
+	@GetMapping("/buffet/{id}")
+	  public String getBuffet(@PathVariable("id") Long id, Model model) {
+	    model.addAttribute("buffet", this.bs.FindById(id));
+	    return "buffet.html";
+
+}
+	@GetMapping("/buffetForm")
+	public String geBuffet(Model model) {
+		model.addAttribute("buffet", new Buffet());
+		return "buffetForm.html";
+		
+	}
+	
+	
 
 }
